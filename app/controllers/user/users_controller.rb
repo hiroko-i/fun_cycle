d9fee587e816e11137235abe129428c16d8ce5bb
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2021-12-08 08:52:52 +0000

    [Add]ユーザー編集

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,6 +1,6 @@
 class User::UsersController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -8,6 +8,14 @@ class User::UsersController < ApplicationController
   end
 
   def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "プロフィールを変更しました"
+    else
+      render "edit"
+    end
   end
 
   def index
@@ -17,5 +25,12 @@ class User::UsersController < ApplicationController
 
   def user_params
     params.require(@user).permit(:nickname, :profile_image, :description, :prefecture, :bicycle_model, :average_distance, :how_to_enjoy)
+  end
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
