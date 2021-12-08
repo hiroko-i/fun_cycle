c9098cccd9cdd6e4d3ae58dc43a86b6debf50015
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2021-12-08 06:06:38 +0000

    header/footer

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,10 +1,21 @@
 class User::UsersController < ApplicationController
+  before_action :authenticate_user!
+
+
   def show
+    @user = User.find(params[:id])
+    # @post = @user.post.page
   end
 
   def edit
   end
 
   def index
+  end
+
+  private
+
+  def user_params
+    params.require(@user).permit(:nickname, :profile_image, :description, :prefecture, :bicycle_model, :average_distance, :how_to_enjoy)
   end
 end
