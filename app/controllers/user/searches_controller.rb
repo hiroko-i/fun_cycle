82126e8beb6d58199ca40eae489a49b9631e0360
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2021-12-22 08:51:23 +0000

    [update]ユーザーレイアウト

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -2,13 +2,12 @@ class User::SearchesController < ApplicationController
 
   def user_index
     @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true)
-    
+    @users = @q.result(distinct: true).page(params[:page]).per(12)
   end
 
   def post_index
     @q = Post.ransack(params[:q])
-    @posts = @q.result(distinct: true)
+    @posts = @q.result(distinct: true).page(params[:page]).per(12)
   end
 
 end
