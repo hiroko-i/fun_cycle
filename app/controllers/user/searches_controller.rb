01bbfa027d4297612b93bef74c9c1d4a20599f39
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2021-12-26 10:09:22 +0000

    [Update]検索結果降順

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -2,12 +2,12 @@ class User::SearchesController < ApplicationController
 
   def user_index
     @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true).page(params[:page]).per(12)
+    @users = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(12)
   end
 
   def post_index
     @q = Post.ransack(params[:q])
-    @posts = @q.result(distinct: true).page(params[:page]).per(12)
+    @posts = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(12)
   end
 
   def post_map
